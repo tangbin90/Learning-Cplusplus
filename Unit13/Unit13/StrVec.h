700fa7d6d356b1eb596e63f0317eeaa4151cedc9
TangBin <tangbin90@gmail.com>
2016-03-23 21:41:20 +0800

    Unit13 13.5
    StrVec class

---
+++ Unit13/Unit13/StrVec.h
@@ -0,0 +1,38 @@
+#ifndef STRVEC_H
+#define STRVEC_H
+#include <string>
+
+class StrVec
+{
+public:
+	StrVec() :
+		elements(nullptr), first_free(nullptr), cap(nullptr){}
+	StrVec(const StrVec&);
+	StrVec& operator=(const StrVec&);
+	~StrVec();
+	void push_back(const std::string&);
+	size_t size() const{ return cap - elements; };
+	size_t capacity() const{ return cap - elements; }
+	std::string* begin() const { return elements; }
+	std::string* end() const { return first_free; }
+	void reserve(size_t size);
+	size_t capacity();
+	size_t size();
+	void resize(size_t size);
+	void resize(size_t size, std::string& T);
+private:
+	static std::allocator<std::string> alloc;
+
+	void chk_n_alloc()
+	{
+		if (size() == capacity()) reallocate();
+	}
+	std::pair<std::string*, std::string*> alloc_n_copy(const std::string*, const std::string*);
+	void free();
+	void reallocate();
+	std::string* elements;
+	std::string* first_free;
+	std::string* cap;
+};
+
+#endif
