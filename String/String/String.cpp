70de7280313f71cf35d5a2a336e3faad058020ac
TangBin <tangbin90@gmail.com>
2016-03-25 21:34:08 +0800

    Class String

---
+++ String/String/String.cpp
@@ -0,0 +1,97 @@
+// String.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "String.h"
+#include <algorithm>
+#include <memory>
+#include <iostream>
+#include <vector>
+void String::reallocate()
+{
+	auto newcapacity = size() ? size() * 2 : 1;
+	auto newdata = alloc.allocate(newcapacity);
+	auto dest = newdata;
+	auto ele = elements;
+	for (size_t i = 0; i != size(); ++i)
+	{
+		alloc.construct(dest++, *ele++);
+	}
+	free();
+	elements = newdata;
+	firstfree = dest;
+	cap = newdata + newcapacity;
+}
+
+void String::free()
+{
+	if (elements)
+	{
+		std::for_each(elements, firstfree, [this](char& a){this->alloc.destroy(&a); });
+		alloc.deallocate(elements, cap - elements);
+	}
+
+}
+
+std::pair<char*, char*> String::alloc_n_copy(const char* begin, const char* end)
+{
+
+	auto data = alloc.allocate(end - begin);
+	return{ data, std::uninitialized_copy(begin, end, data) };
+}
+
+String::~String()
+{
+	free();
+}
+
+String::String(const char* begin)
+{
+	int length = strlen(begin);
+	char* end = const_cast<char*>(begin)+length+1;
+	auto data = alloc_n_copy(begin, end);
+	elements = data.first;
+	firstfree = cap = data.second;
+}
+
+String::String(const String& str)
+{
+	auto newdata = alloc_n_copy(str.begin(), str.end());
+	elements = newdata.first;
+	firstfree = cap = newdata.second;
+	std::cout << "copy construct" << std::endl;
+}
+
+String& String::operator=(String& str)
+{
+	auto newdata = alloc_n_copy(str.begin(), str.end());
+	free();
+	elements = newdata.first;
+	firstfree = cap = newdata.second;
+	std::cout << "Operator=" << std::endl;
+	return *this;
+}
+
+String& String::operator+(String& str)
+{
+	 
+}
+
+std::ostream& operator<<(std::ostream& out, String& str)
+{
+	out << str.elements << std::endl;
+	return out;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::vector<String> vecstr;
+	String str1("tangbin");
+	vecstr.push_back(str1);
+	vecstr.push_back("good job");
+	//vecstr.push_back(str2);
+	//vecstr.push_back(str3);
+	system("pause");
+	return 0;
+}
+
---
+++ String/String/String.h
@@ -0,0 +1,39 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include <memory>
+#include <string>
+#include <iostream>
+class String
+{
+	friend std::ostream& operator<<(std::ostream& out, String& str);
+public:
+	String() :String(""){}
+	String(const char*);
+	String(const String& str);//复制构造函数一定要声明const
+	String& operator=(String&);
+	String& operator+(String&);
+	size_t size()const { return firstfree - elements; }
+	size_t capacity()const { return cap - elements; }
+	char* begin()const{ return elements; }
+	char* end()const { return firstfree; }
+
+	~String();
+	void push_back(const char);
+	
+private:
+	void chk_n_alloc()
+	{
+		if (size() == capacity()) reallocate();
+	}
+	std::pair<char*, char*> alloc_n_copy(const char*, const char*);
+	void free();
+	void reallocate();
+	char *elements;
+	char *firstfree;
+	char *cap;
+	std::allocator<char> alloc;
+
+};
+
+#endif
